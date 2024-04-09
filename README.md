03be069eb19bb0d5b4eb0c59b320a312f1e59ed7
Leif Denby <leif@denby.eu>
2024-04-09 11:21:21 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,77 @@
+# mllam-data-interface
+
+**Simple interfaces for describing weather machine learning datasets**
+
+The purpose of `mllam_data_interface` is to provide a simple way to:
+
+- describe a specification for what variables and attributes a specific
+  `pytorch.Dataset` expects a `zarr`-based training dataset to contain
+- check a `zarr` training-dataset against a specification
+- provide storage for a collection of dataset specifications (these are in
+  [specs/](specs/) with path format `specs/{spec_name}/{spec_version}.yaml`)
+
+# Usage
+
+The intention with this package is that you can use it both when a) creating a
+dataset and b) loading a dataset to check that the dataset contains what it
+needs to. For convenience you can also [run it from the command line](#usage-from-command-line).
+
+## Usage when loading a dataset
+
+E.g. when loading a dataset:
+
+```python
+import mllam_data_interface as mdi
+import xarray as xr
+
+class MyWeatherDataset(pytorch.Dataset):
+    mllam_spec = "neural_lam:v0.1.0"
+
+    def __init__(self, dataset_path):
+        self.ds = xr.open_zarr(self.dataset_path)
+        mdi.check_dataset(ds=self.ds, spec_identifier=self.mllam_spec)
+```
+
+## Usage from command line
+
+To check that a training dataset matches a spec you can use
+`mllam_data_interface` from the command line:
+
+```bash
+python -m mllam_data_interface.check my_dataset.zarr neural_lam:v0.1.0
+```
+
+Which will output something like (if the dataset matches the spec):
+
+```bash
+python -m mllam_data_interface.check ../mllam-data-prep/example.danra.zarr neural_lam:v0.1.0
+2024-04-09 11:04:21.943 | INFO     | __main__:<module>:72 - Opening Zarr file at example.danra.zarr
+2024-04-09 11:04:22.023 | INFO     | __main__:<module>:76 - Dataset matches the spec!
+```
+
+Or (if the dataset doesn't match the spec):
+
+```bash
+2024-04-09 11:06:11.439 | INFO     | __main__:<module>:72 - Opening Zarr file at example.danra.incomplete.zarr
+2024-04-09 11:06:11.518 | ERROR    | __main__:<module>:79 - Variable static is missing from the dataset.
+```
+
+## Specification format
+
+All specifications are given as `yaml`-files. Currently, the specifications allow you to specify:
+
+1. Which variables the dataset must contain, and which dimensions each should have (the order of the dimensions is also checked)
+2. Which attributes the dataset must have
+
+For example, the spec `neural_lam:v0.1.0` specifies that the dataset must contain the variables `static`, `state` and `forcing` variables could be written as:
+
+```yaml
+variables:
+  static:
+    dims: [grid_index, feature]
+  state:
+    dims: [time, grid_index, state_feature]
+  forcing:
+    dims: [time, grid_index, forcing_feature]
+attributes: [version]
+```
---
+++ src/mllam_data_interface/check.py
@@ -0,0 +1,90 @@
+from pathlib import Path
+
+import xarray as xr
+import yaml
+
+SPECS_ROOT_PATH = Path(__file__).parent.parent.parent / "specs"
+
+
+def load_spec(spec_identifier: str):
+    """
+    Load dataset specification from the given identifier.
+    The specification is a string with format `{spec_name}:{spec_version}`.
+
+    Parameters
+    ----------
+    spec_identifier : str
+        The dataset specification identifier, e.g. `neural_lam:v0.1.0`
+
+    Returns
+    -------
+    dict
+        The dataset specification.
+    """
+    if ":" not in spec_identifier:
+        raise ValueError(
+            "The spec_identifier must be in the format {spec_name}:{spec_version}"
+        )
+
+    spec_name, spec_version = spec_identifier.split(":")
+
+    fp = SPECS_ROOT_PATH / spec_name / f"{spec_version}.yaml"
+
+    if not fp.exists():
+        raise ValueError(f"Spec file {fp} for {spec_identifier} does not exist.")
+
+    with open(fp, "r") as f:
+        spec = yaml.safe_load(f)
+
+    return spec
+
+
+def check_dataset(ds: xr.Dataset, spec_identifier: str):
+    """
+    Check that xr.Dataset `ds` satisfies the specification `spec_identifier` by
+    container the necessary variables, dimensions and attributes.
+    """
+
+    spec = load_spec(spec_identifier=spec_identifier)
+
+    required_variables = spec.get("variables", {})
+
+    for var_name, var_parts in required_variables.items():
+        if var_name not in ds.data_vars:
+            raise ValueError(f"Variable {var_name} is missing from the dataset.")
+
+        dims = var_parts.get("dims", [])
+
+        if not all(dim in ds[var_name].dims for dim in dims):
+            raise ValueError(
+                f"Variable {var_name} does not have the required dimensions {dims}."
+            )
+
+    required_attributes = spec.get("attributes", [])
+
+    for attr in required_attributes:
+        if attr not in ds.attrs:
+            raise ValueError(f"Attribute {attr} is missing from the dataset.")
+
+
+if __name__ == "__main__":
+    import argparse
+
+    from loguru import logger
+
+    argparser = argparse.ArgumentParser(description="MLLAM Data Interface")
+    argparser.add_argument("zarr_filepath", help="Path to the Zarr file to check")
+    argparser.add_argument(
+        "spec_identifier",
+        help="The identifier of the spec to check, e.g. `neural_lam:v0.1.0`",
+    )
+    args = argparser.parse_args()
+
+    logger.info(f"Opening Zarr file at {args.zarr_filepath}")
+    ds = xr.open_zarr(args.zarr_filepath)
+    spec_identifier = args.spec_identifier
+    try:
+        check_dataset(ds=ds, spec_identifier=spec_identifier)
+        logger.info("Dataset matches the spec!")
+    except Exception as ex:
+        logger.exception(ex)
